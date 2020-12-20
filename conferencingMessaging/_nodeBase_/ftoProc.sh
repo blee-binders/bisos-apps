f0c166e187c551a9108fdd2d002a274e3bb05b03
Mohsen Banan <git@mohsen.1.banan.byname.net>
2020-12-19 16:06:44 -0800

    additions and dblock updates.

--- conferencingMessaging/_nodeBase_/ftoProc.sh
+++ conferencingMessaging/_nodeBase_/ftoProc.sh
@@ -16,7 +16,7 @@ SEED="
 *  /[dblock]/ /Seed/ :: [[file:/bisos/core/bsip/bin/seedFtoCommon.sh]] | 
 "
 FILE="
-*  /This File/ :: /bisos/panels/bisos-apps/ftoProc.sh 
+*  /This File/ :: /bisos/panels/bisos-apps/conferencingMessaging/_nodeBase_/ftoProc.sh 
 "
 if [ "${loadFiles}" == "" ] ; then
     /bisos/core/bsip/bin/seedFtoCommon.sh -l $0 "$@" 
@@ -53,13 +53,6 @@ _CommentEnd_
 ####+BEGIN: bx:dblock:ploneProc:bash:nodesList :types ""
 # {{{ DBLOCK-nodesList
 nodesList="
-roInvofier
-conferencingMessaging
-lcnt
-_nodeBase_
-marmee
-multimedia
-browsers
 "
 # }}} DBLOCK-nodesList
 ####+END:
--- conferencingMessaging/ftoProc.sh
+++ conferencingMessaging/ftoProc.sh
@@ -16,7 +16,7 @@ SEED="
 *  /[dblock]/ /Seed/ :: [[file:/bisos/core/bsip/bin/seedFtoCommon.sh]] | 
 "
 FILE="
-*  /This File/ :: /bisos/panels/bisos-apps/ftoProc.sh 
+*  /This File/ :: /bisos/panels/bisos-apps/conferencingMessaging/ftoProc.sh 
 "
 if [ "${loadFiles}" == "" ] ; then
     /bisos/core/bsip/bin/seedFtoCommon.sh -l $0 "$@" 
@@ -41,6 +41,9 @@ _CommentEnd_
 ####+BEGIN: bx:dblock:ploneProc:bash:leavesList :types ""
 # {{{ DBLOCK-leavesList
 leavesList="
+zoom
+skype
+whatsApp
 "
 # }}} DBLOCK-leavesList
 ####+END:
@@ -53,13 +56,7 @@ _CommentEnd_
 ####+BEGIN: bx:dblock:ploneProc:bash:nodesList :types ""
 # {{{ DBLOCK-nodesList
 nodesList="
-roInvofier
-conferencingMessaging
-lcnt
 _nodeBase_
-marmee
-multimedia
-browsers
 "
 # }}} DBLOCK-nodesList
 ####+END:
