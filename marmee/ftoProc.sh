2c1878fed041c9d8a74cbc96c7bb33bde8a6f69c
Mohsen Banan <form@mohsen.banan.1.byname.net>
2022-10-23 17:30:14 -0700

    Restructured marmee panel to reflect code.

--- marmee/ftoProc.sh
+++ marmee/ftoProc.sh
@@ -13,13 +13,13 @@ __copying__="
 
 ####+BEGIN: bx:bsip:bash:seed-spec :types "seedFtoCommon.sh"
 SEED="
-*  /[dblock]/ /Seed/ :: [[file:/bisos/core/bsip/bin/seedFtoCommon.sh]] | 
+*  /[dblock]/ /Seed/ :: [[file:/bisos/core/bsip/bin/seedFtoCommon.sh]] |
 "
 FILE="
-*  /This File/ :: /bisos/panels/bisos-apps/marmee/ftoProc.sh 
+*  /This File/ :: /bisos/git/auth/bxRepos/blee-binders/bisos-apps/marmee/ftoProc.sh
 "
 if [ "${loadFiles}" == "" ] ; then
-    /bisos/core/bsip/bin/seedFtoCommon.sh -l $0 "$@" 
+    /bisos/core/bsip/bin/seedFtoCommon.sh -l $0 "$@"
     exit $?
 fi
 ####+END:
@@ -41,7 +41,6 @@ _CommentEnd_
 ####+BEGIN: bx:dblock:ploneProc:bash:leavesList :types ""
 # {{{ DBLOCK-leavesList
 leavesList="
-bx822-qmail-remote
 "
 # }}} DBLOCK-leavesList
 ####+END:
@@ -58,11 +57,15 @@ _CommentEnd_
 ####+BEGIN: bx:dblock:ploneProc:bash:nodesList :types ""
 # {{{ DBLOCK-nodesList
 nodesList="
-installConfig
-_nodeBase_
-outGoingMail
+install
 dsnProc
+_nodeBase_
+mailCreds
+daemonization
 inComingMail
+pyApi
+config
+outGoingMail
 "
 # }}} DBLOCK-nodesList
 ####+END:
