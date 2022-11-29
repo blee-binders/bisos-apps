08c641c86fbd9164e680f8fc7b31a09008e48183
Mohsen Banan <form@mohsen.banan.1.byname.net>
2022-11-28 22:15:56 -0800

    A whole lot more rearranging.

--- marmee/notmuch/ftoProc.sh
+++ marmee/notmuch/ftoProc.sh
@@ -16,7 +16,7 @@ SEED="
 *  /[dblock]/ /Seed/ :: [[file:/bisos/core/bsip/bin/seedFtoCommon.sh]] |
 "
 FILE="
-*  /This File/ :: /bisos/git/auth/bxRepos/blee-binders/bisos-apps/marmee/inComingMail/ftoProc.sh
+*  /This File/ :: /bisos/git/auth/bxRepos/blee-binders/bisos-apps/marmee/notmuch/ftoProc.sh
 "
 if [ "${loadFiles}" == "" ] ; then
     /bisos/core/bsip/bin/seedFtoCommon.sh -l $0 "$@"
